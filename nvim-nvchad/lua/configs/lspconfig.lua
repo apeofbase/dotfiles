cc0cec066ca0c7e5b85750c07857aadf0e312edd
Justin Gable <justin@justingable.com>
2024-11-04 12:01:00 -0800

    Add JS and YAML LSP support

--- nvim-nvchad/lua/configs/lspconfig.lua
+++ nvim-nvchad/lua/configs/lspconfig.lua
@@ -27,12 +27,27 @@ lspconfig.twiggy_language_server.setup {
   capabilities = capabilities,
   settings = {
     twiggy = {
-      framework = 'symfony',
-      phpExecutable = '/usr/bin/php',
-      symfonyConsolePath = 'bin/console',
+      framework = "symfony",
+      phpExecutable = "/usr/bin/php",
+      symfonyConsolePath = "bin/console",
     },
   },
 }
+
+lspconfig.yamlls.setup {
+  -- other configuration for setup {}
+  settings = {
+    yaml = {
+      -- other settings. note this overrides the lspconfig defaults.
+      schemas = {
+        -- ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+        -- ["../path/relative/to/file.yml"] = "/.github/workflows/*",
+        -- ["/path/from/root/of/project"] = "/.github/workflows/*",
+      },
+    },
+  },
+}
+
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
 --   on_attach = nvlsp.on_attach,
