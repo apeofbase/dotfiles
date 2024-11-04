17512a34280e6e52be7ad98b5145e1310daffe07
Justin Gable <justin@justingable.com>
2024-11-04 12:01:00 -0800

    Add JS and YAML LSP support

--- nvim-nvchad/lua/configs/conform.lua
+++ nvim-nvchad/lua/configs/conform.lua
@@ -3,6 +3,8 @@ local options = {
     lua = { "stylua" },
     css = { "prettier" },
     html = { "prettier" },
+    javascript = { "prettier" },
+    yaml = { "prettier" },
   },
 
   -- format_on_save = {
