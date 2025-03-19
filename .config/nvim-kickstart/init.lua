eff5eef7259fa864d9a573f735d5e5130a711d9d
Justin Gable <justin@justingable.com>
2025-03-18 18:53:12 -0700

    Neovim Kickstart

---
+++ .config/nvim-kickstart/init.lua
@@ -0,0 +1,63 @@
+--[[
+
+=====================================================================
+==================== READ THIS BEFORE CONTINUING ====================
+=====================================================================
+========                                    .-----.          ========
+========         .----------------------.   | === |          ========
+========         |.-""""""""""""""""""-.|   |-----|          ========
+========         ||                    ||   | === |          ========
+========         ||   KICKSTART.NVIM   ||   |-----|          ========
+========         ||                    ||   | === |          ========
+========         ||                    ||   |-----|          ========
+========         ||:Tutor              ||   |:::::|          ========
+========         |'-..................-'|   |____o|          ========
+========         `"")----------------(""`   ___________      ========
+========        /::::::::::|  |::::::::::\  \ no mouse \     ========
+========       /:::========|  |==hjkl==:::\  \ required \    ========
+========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
+========                                                     ========
+=====================================================================
+=====================================================================
+
+--]]
+
+-- [[ Initial requires ]]
+require 'kickstart.plugins.debug'
+require 'custom.options'
+require 'custom.mappings'
+require 'custom.autocommands'
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+-- [[ Configure and install plugins ]]
+require('lazy').setup({
+
+  -- require 'kickstart.plugins.debug',
+  -- require 'kickstart.plugins.indent_line',
+  -- require 'kickstart.plugins.lint',
+  -- require 'kickstart.plugins.autopairs',
+  -- require 'kickstart.plugins.neo-tree',
+  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+
+  -- User plugins
+  -- Include all custom plugins with more config
+  { import = 'custom.plugins' },
+}, {
+  ui = {
+    icons = {},
+  },
+})
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,5 +1,7 @@
 return {
   {
+    -- Lightweight yet powerful formatter plugin
+    -- https://github.com/stevearc/conform.nvim
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
@@ -15,21 +17,29 @@ return {
   -- User plugins
   --
   {
+    -- Mappings to easily delete, change and add surroundings in pairs
+    -- https://github.com/tpope/vim-surround
     "tpope/vim-surround",
     lazy = false
   },
 
   {
+    -- Autopair plugin that supports multiple characters
+    -- https://github.com/windwp/nvim-autopairs
     'windwp/nvim-autopairs',
     event = "InsertEnter"
   },
 
   {
+    -- Delete buffers (close files) without closing your windows
+    -- https://github.com/moll/vim-bbye
     "moll/vim-bbye",
     lazy = false
   },
 
   {
+    -- Better Quickfix
+    -- https://github.com/kevinhwang91/nvim-bqf
     "kevinhwang91/nvim-bqf",
     lazy = false
   },
