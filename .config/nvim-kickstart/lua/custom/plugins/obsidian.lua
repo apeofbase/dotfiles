43a3e8dfb95a1a008e2ab1ad2b60067c45a2e84c
Justin Gable <justin@justingable.com>
2025-07-09 11:32:46 -0700

    Update Obsidian.nvim to new fork with Blink support

--- .config/nvim-kickstart/lua/custom/plugins/obsidian.lua
+++ .config/nvim-kickstart/lua/custom/plugins/obsidian.lua
@@ -1,10 +1,13 @@
 return {
   {
-    'epwalsh/obsidian.nvim',
-    version = 'v3.7.*',
+    'obsidian-nvim/obsidian.nvim',
+    version = 'v3.11.*',
     ft = 'markdown',
     lazy = false,
     opts = {
+      completion = {
+        blink = true,
+      },
       event = {
         'BufReadPre ' .. vim.fn.expand '~' .. '/Vaults/**.md',
         'BufNewFile ' .. vim.fn.expand '~' .. '/Vaults/**.md',
