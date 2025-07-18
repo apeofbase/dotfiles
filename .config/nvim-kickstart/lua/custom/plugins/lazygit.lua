697d3b8dfda0fb24ba6165545936c183d25b2aed
Justin Gable <justin@justingable.com>
2025-07-18 15:50:22 -0700

    Wezterm spacing and obsidian backport

--- .config/nvim-kickstart/lua/custom/plugins/lazygit.lua
+++ .config/nvim-kickstart/lua/custom/plugins/lazygit.lua
@@ -3,9 +3,6 @@ return {
     'kdheepak/lazygit.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
     cmd = 'LazyGit',
-    -- config = function()
-    --   vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>", { desc = "[L]azy[G]it" })
-    -- end,
     keys = {
       { '<leader>tl', '<cmd>LazyGit<CR>', desc = '[T]oggle [L]azyGit' },
     },
