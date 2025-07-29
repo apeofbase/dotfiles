bc2f86b092621c25984d9a2e9b715c3c4a01a8ed
Justin Gable <justin@justingable.com>
2025-07-28 19:34:56 -0700

    Remove ripgrep autocomplete

--- .config/nvim/lua/custom/plugins/blink.lua
+++ .config/nvim/lua/custom/plugins/blink.lua
@@ -31,7 +31,6 @@ return {
 
           -- Custom sources
           -- 'jdrupal-dev/css-vars.nvim',
-          'mikavilpas/blink-ripgrep.nvim',
         },
       },
       'xzbdmw/colorful-menu.nvim',
@@ -103,32 +102,19 @@ return {
                   return require('colorful-menu').blink_components_highlight(ctx)
                 end,
               },
-              kind = {
-                text = function(ctx)
-                  -- Fix for "RipgrepRipgrep"
-                  if ctx.source_name == 'Ripgrep' then
-                    return 'rg'
-                  end
-                  return ctx.kind
-                end,
-              },
             },
           },
         },
       },
 
       sources = {
-        default = { 'lsp', 'snippets', 'path', 'lazydev', 'buffer', 'ripgrep' },
+        default = { 'lsp', 'snippets', 'path', 'lazydev', 'buffer' },
         per_filetype = {
           codecompanion = { 'codecompanion' },
         },
         providers = {
           lsp = {
             fallbacks = { }
-          },
-          ripgrep = {
-            module = 'blink-ripgrep',
-            name = 'Ripgrep',
           },
           lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
           buffer = {
